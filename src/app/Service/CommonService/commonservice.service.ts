6f9dadd3f6379ac77d1addc41766d01c13f05c50
dapchanvabatquat <khanhson89@gmail.com>
2022-10-12 15:22:58 +0700

    Update commonservice
    cập nhật link API online

--- src/app/Service/CommonService/commonservice.service.ts
+++ src/app/Service/CommonService/commonservice.service.ts
@@ -10,8 +10,8 @@ export class CommonserviceService {
 
   // _urlApi: string = environment.urlApi + '/api/';
   //  _urlApi: string = '45.124.94.191:8090/api/';
-  // public _urlApi: string = 'http://45.124.94.191:8090/api/';
-  public _urlApi: string = 'http://localhost:1195/api/';
+  public _urlApi: string = 'http://45.124.94.191:8090/api/';
+  // public _urlApi: string = 'http://localhost:1195/api/';
   public static Token: string = "";
 
   constructor(
